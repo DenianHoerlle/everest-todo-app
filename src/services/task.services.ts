b7e56887eb630923c5fe61fdc118b8eddd4fdf20
DenianHoerlle <deniancloss@gmail.com>
2025-10-14 18:33:41 -0300

    refactor(core): :label: added api response type

--- src/services/task.services.ts
+++ src/services/task.services.ts
@@ -1,8 +1,10 @@
+import { ApiRequest } from "types";
+
 import api from "./api";
 
 // TODO add error handling
 const getTaskList = async () => {
-  const response = await api.get("/input.json");
+  const response = await api.get<ApiRequest>("/input.json");
 
   return response;
 };
