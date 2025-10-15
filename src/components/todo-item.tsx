c22c21ccf8153305fccf24886a28860e7d89035d
DenianHoerlle <deniancloss@gmail.com>
2025-10-15 14:56:47 -0300

    refactor(core): :recycle: improved todo item performance

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { ChangeEvent, memo, useCallback } from "react";
 
 import useTodoStore from "store";
 import { TodoEntry } from "types";
@@ -7,12 +7,25 @@ import { TodoEntry } from "types";
 const { checkTodo, deleteTodo } = useTodoStore.getState();
 
 const TodoItem = ({ checked, content, id }: TodoEntry) => {
+  const handleOnChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (!event.target.checked) return;
+
+      checkTodo(id);
+    },
+    [id],
+  );
+
+  const handleDelete = useCallback(() => {
+    deleteTodo(id);
+  }, [id]);
+
   return (
     <div key={id} className="flex gap-3">
       <span>{id}</span>
       <span>{content}</span>
-      <button onClick={() => deleteTodo(id)}>delete</button>
-      <input type="checkbox" checked={checked} onChange={() => checkTodo(id)} />
+      <button onClick={handleDelete}>delete</button>
+      <input type="checkbox" checked={checked} onChange={handleOnChange} />
     </div>
   );
 };
