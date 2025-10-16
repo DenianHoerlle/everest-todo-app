d01ba7dd93d74c303cef9c77c01c094a9b880968
DenianHoerlle <deniancloss@gmail.com>
2025-10-16 12:04:20 -0300

    feat(core): :heavy_plus_sign: added tailwind plugin dev dependency

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,14 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+
 const config = {
   arrowParens: "avoid",
   bracketSameLine: true,
   bracketSpacing: true,
   singleQuote: false,
   trailingComma: "all",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/index.css",
+  tailwindFunctions: ["/className/gmi"],
 };
 
 export default config;
