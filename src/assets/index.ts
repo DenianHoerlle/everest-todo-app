7694c00c90316c2e6190b212bdf471daa40c61db
DenianHoerlle <deniancloss@gmail.com>
2025-10-17 23:49:59 -0300

    style(core): :art: added plus icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import CheckMark from "./icons/check-mark.svg";
+import Plus from "./icons/plus.svg";
 import Thrash from "./icons/thrash.svg";
 
-export { CheckMark, Thrash };
+export { CheckMark, Plus, Thrash };
