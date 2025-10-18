2b4ab55cf0f42a692ccf3d74c73ccbcae23e3249
DenianHoerlle <deniancloss@gmail.com>
2025-10-18 11:19:07 -0300

    refactor(core): :recycle: improved delete icon readability

--- src/components/todo/components/delete.tsx
+++ src/components/todo/components/delete.tsx
@@ -5,10 +5,14 @@ type AnimateValues = {
   offset: number;
 };
 
+// TODO try (again) to source colors from tailwind (index.css) file
+const everRed = "#f96673";
+const everGrey = "#9fbdcd";
+
 const animateValues: AnimateValues[] = [
-  { values: "#9fbdcd; #f96673; #f96673; #f96673;", offset: 0 },
-  { values: "#9fbdcd; #9fbdcd; #f96673; #f96673;", offset: 0.5 },
-  { values: "#9fbdcd; #9fbdcd; #9fbdcd; #f96673;", offset: 1 },
+  { values: `${everGrey}; ${everRed}; ${everRed}; ${everRed};`, offset: 0 },
+  { values: `${everGrey}; ${everGrey}; ${everRed}; ${everRed};`, offset: 0.5 },
+  { values: `${everGrey}; ${everGrey}; ${everGrey}; ${everRed};`, offset: 1 },
 ];
 
 const ThrashIconSVG = (isAnimating: boolean, id: number) => {
@@ -27,7 +31,7 @@ const ThrashIconSVG = (isAnimating: boolean, id: number) => {
 
   const renderAnimatedGradient = (data: AnimateValues) => {
     return (
-      <stop offset={data.offset} stop-color="#f96673">
+      <stop offset={data.offset} stopColor={everRed} key={data.offset}>
         <animate
           attributeName="stop-color"
           begin="indefinite"
@@ -56,19 +60,21 @@ const ThrashIconSVG = (isAnimating: boolean, id: number) => {
       data-name="animated-thrash"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 40.03 46.06"
-      width="16px">
+      width="16px"
+    >
       <defs>
         <linearGradient
           id={`appear-gradient-${id}`}
           x1="0.5"
           x2="0.5"
           y1="1"
-          y2="0">
+          y2="0"
+        >
           {animateValues.map(renderAnimatedGradient)}
         </linearGradient>
       </defs>
       <path
-        fill={isAnimating ? `url(#appear-gradient-${id})` : "#9fbdcd"}
+        fill={isAnimating ? `url(#appear-gradient-${id})` : everGrey}
         d="M14.46.22c-1.71.31-1.82 2.63-2.45 2.85-3.41.29-7.21-.29-10.58.02C.69 3.16.21 3.28.08 4.12c-.09.61-.11 3.06 0 3.64.09.49.6.82 1.04.97l38.03-.1c.26-.17.65-.41.74-.72.21-.72.33-4.45-.68-4.73L27.98 3.07C27.3 2.84 27.29.46 25.37.19c-1.72-.24-9.28-.27-10.91.03ZM3.08 11.52c-.54.36-.11.89-.09 1.37.31 10.02 1.24 20.07 1.95 30.04.48 1.6 1.99 2.74 3.62 2.96 7.31-.44 15.27.56 22.5 0 1.99-.16 3.57-1.3 4.06-3.26.61-9.89 1.57-19.83 1.88-29.75.01-.25.41-1.37-.09-1.37H3.09Z"
       />
       {isAnimating && renderAnimatedWiggle()}
@@ -90,7 +96,7 @@ const DeleteIcon = ({ onClick, id }: { onClick: () => void; id: number }) => {
   const handleOnMouseDown = () => {
     setIsAnimating(true);
     timer.current = setTimeout(() => {
-      onClick;
+      onClick();
     }, 1000);
   };
 
@@ -101,7 +107,9 @@ const DeleteIcon = ({ onClick, id }: { onClick: () => void; id: number }) => {
       onMouseDown={handleOnMouseDown}
       onMouseLeave={handleCancelDelete}
       onMouseUp={handleCancelDelete}
-      className={`container ml-auto flex h-6 w-6 cursor-pointer items-center justify-center transition ${buttonAnimation}`}>
+      className={`container ml-auto flex h-6 w-6 cursor-pointer items-center justify-center transition ${buttonAnimation}`}
+      key={id}
+    >
       {ThrashIconSVG(isAnimating, id)}
     </button>
   );
