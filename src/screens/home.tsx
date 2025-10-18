bc724d7a84eb760de5960fae1910e9e6727790b3
DenianHoerlle <deniancloss@gmail.com>
2025-10-18 16:00:54 -0300

    feat(core): :sparkles: added animation to todo list

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,17 +1,31 @@
+import { startTransition, useEffect, useState, ViewTransition } from "react";
+
 import { Form, Header, Todo } from "components";
 import useTodoStore from "store";
 
 const HomeScreen = () => {
   const todos = useTodoStore(state => state.todos);
 
+  const [orderedTodos, setOrderedTodos] = useState(todos);
+
+  useEffect(() => {
+    startTransition(() => {
+      setOrderedTodos(todos);
+    });
+  }, [todos]);
+
   return (
     <div className="mt-20">
       <Header />
       <Form />
       <div className="mx-auto flex h-[300vh] max-h-[90vh] w-xl flex-col items-start gap-4">
-        {todos.map(todo => (
-          <Todo key={todo.id} {...todo} />
-        ))}
+        {orderedTodos.map(todo => {
+          return (
+            <ViewTransition key={todo.id}>
+              <Todo {...todo} />
+            </ViewTransition>
+          );
+        })}
       </div>
     </div>
   );
