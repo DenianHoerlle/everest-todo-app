bc6fae3f5bc5b58cdcf66d5c8212cf3a4791ae88
DenianHoerlle <deniancloss@gmail.com>
2025-10-19 17:08:04 -0300

    chore(extra): :art: added github and linkedin images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,8 @@
 import CheckMark from "./icons/check-mark.svg";
+import Github from "./icons/github.png";
+import Linkedin from "./icons/linkedin.png";
 import Plus from "./icons/plus.svg";
 import SearchIcon from "./icons/search.svg";
 import Thrash from "./icons/thrash.svg";
 
-export { CheckMark, Plus, SearchIcon, Thrash };
+export { CheckMark, Github, Linkedin, Plus, SearchIcon, Thrash };
